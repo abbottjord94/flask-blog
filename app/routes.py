97e0759b2581f6f078c1be67d27c734486903784
Jordan Abbott <abbottjord94@gmail.com>
2020-01-23 20:07:07 -0800

    modified:   app/__pycache__/routes.cpython-36.pyc 	modified:   app/routes.py# 	modified:   app/site/index.html

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,25 @@
 from app import app
 from flask import render_template
 
+items = [
+	{
+		'author': "Jordan Abbott",
+		'text': "Epstein Didn't Kill Himself"
+	},
+	{
+		'author': "Jordan Abbott",
+		'text': "All hail our mightly robot overlords"
+	}
+]
+
 @app.route('/')
 def home():
-	return render_template('index.html')
+	return render_template('index.html', posts=items)
 
 @app.route('/about')
 def about():
-	return "<h1>About me:</h1>"
+	return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
